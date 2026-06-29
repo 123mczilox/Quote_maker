4db526ed9a6eeb6da57fe76b0e2aff4b7007e16e
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 <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>
2026-06-29 17:38:54 +0300

    done 1

--- Quote_maker/settings.py
+++ Quote_maker/settings.py
@@ -27,9 +27,11 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [
+    'quotemaker-production-b065.up.railway.app'
+]
 
 
 
